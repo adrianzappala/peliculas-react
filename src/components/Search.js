cb3680d0f28ddb7cb0e204a5c8b59776c910c0ab
Adrián Zappala <avz_f@hotmail.com>
2025-06-02 20:06:08 -0300

    Primer commit: proyecto películas en React

---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import styles from "./Search.module.css";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "../hooks/useQuery";
+
+export function Search() {
+  const query = useQuery();
+  const search = query.get("search") ?? "";
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    navigate(`/?search=${value}`);
+  };
+
+  return (
+    <form className={styles.searchContainer} onSubmit={handleSubmit}>
+      <div className={styles.searchBox}>
+        <input
+          className={styles.searchInput}
+          type="text"
+          value={search}
+          autoFocus
+          placeholder="Buscar películas"
+          aria-label="Search Movies"
+          onChange={handleChange}
+        />
+        <FaSearch size={20} color="black" className={styles.searchButton} />
+      </div>
+    </form>
+  );
+}
